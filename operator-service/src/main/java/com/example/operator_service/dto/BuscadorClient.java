2c3778615b164305383ac9b9347f5965c03e423f
Moises <moises.sanchez@tdcon40.com>
2025-08-04 10:38:37 -0600

    changes to inventory-service-url

--- operator-service/src/main/java/com/example/operator_service/dto/BuscadorClient.java
+++ operator-service/src/main/java/com/example/operator_service/dto/BuscadorClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "inventory-service")
+@FeignClient(name = "inventory-service", url = "${INVENTORY_SERVICE_URL}")
 public interface BuscadorClient {
 
     @GetMapping("/api/productos/{id}")
