3624fca2246c856b34009e6eac1c5208ce4c3dbf
Moises <moises.sanchez@tdcon40.com>
2025-08-04 07:32:31 -0600

    UpdatetedCorsConfig

---
+++ gateway-service/src/main/java/com/gym/gateway_service/CorsConfig.java
@@ -0,0 +1,28 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of(
+            "https://tiendagimnasio.vercel.app",
+            "https://tiendagimnasio-moises-projects-e3b753ac.vercel.app"
+        ));
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "PUT", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
