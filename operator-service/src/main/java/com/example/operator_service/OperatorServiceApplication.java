519c5c667b622667b082b7c0a284e025322e4699
Moises <moises.sanchez@tdcon40.com>
2025-08-04 10:27:37 -0600

    ass

--- operator-service/src/main/java/com/example/operator_service/OperatorServiceApplication.java
+++ operator-service/src/main/java/com/example/operator_service/OperatorServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-
+@EnableDiscoveryClient
 public class OperatorServiceApplication {
 
 	public static void main(String[] args) {
