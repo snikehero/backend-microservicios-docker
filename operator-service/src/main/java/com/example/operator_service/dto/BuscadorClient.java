1e74fb6075a5d6956850c8553db17096460170c7
Moises <moises.sanchez@tdcon40.com>
2025-08-04 09:53:29 -0600

    test

--- operator-service/src/main/java/com/example/operator_service/dto/BuscadorClient.java
+++ operator-service/src/main/java/com/example/operator_service/dto/BuscadorClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "inventory-service", url = "https://inventory-production-f2a0.up.railway.app")
+@FeignClient(name = "inventory-service")
 public interface BuscadorClient {
 
     @GetMapping("/api/productos/{id}")
